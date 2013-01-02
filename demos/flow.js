1e4be006c933efd1fc48addc381ef7f350c50e23
Daniel Phillimore <dan@ovms.co>
2013-01-02 15:27:54 +0000

    Added "Flow" programming language demo to documentation

--- demos/flow.js
+++ demos/flow.js
@@ -1,65 +1,15 @@
-service.js
-==========
+/**
+ * ServiceJS library v0.0.1
+ * https://github.com/weejot/service.js
+ *
+ * Copyright 2012 Weejot (weejot.com)
+ *
+ * Released under the NCSA Open Source license
+ * https://github.com/weejot/service.js/raw/master/NCSA-LICENSE.txt
+ */
 
-Service.JS is an Inversion Of Control (IoC) container for JavaScript.
-Similar to Symfony's [Dependency Injection Component (DIC)](http://symfony.com/doc/2.0/components/dependency_injection/introduction.html) it allows you to separate the construction of objects in your application from the rest of the code.
+/*global require, define */
 
-Parameters
-----------
-
-Parameters allow you to the separate the configuration of the options for your services from the service definitions themselves.
-It is possible to use a container just to store parameters, as in the following:
-
-```javascript
-require([
-    "service"  // Path to /vendor/service.js/service.js, with ".js" auto-appended by Modular
-], function (
-    service
-) {
-    "use strict";
-
-    var components = service.create(),
-        container = components.container,
-        objectLoader = components.objectLoader,
-        object = {
-            "parameters": {
-                "majorVersion": 4,
-                "minorVersion": 2,
-                "revision": 1024,
-                "version": "%majorVersion%.%minorVersion%.%revision%"
-            }
-        };
-
-    objectLoader.load(object);
-
-    console.log(container.getParameter("majorVersion")); // Will print "4" to the console
-    console.log(container.getParameter("minorVersion")); // Will print "2" to the console
-    console.log(container.getParameter("revision"));     // Will print "1024" to the console
-    console.log(container.getParameter("version"));      // Will print "4.2.1024" to the console
-});
-```
-
-As can be seen in the example above, it is possible to refer to other parameters from within a string value for another parameter.
-This is achieved by enclosing the name of the referenced parameters in percentage signs, as shown above.
-
-Services
---------
-
-Services allow you to separate the construction of objects for your application from the logic that uses them.
-
-Real-world example
-------------------
-You wish to create a Virtual Machine for a programming language called "Flow". "Flow" will be a superset of ECMAScript, with one addition: a new language construct called "print" has been added:
-
-```javascript
-// ...
-vm.run("print 4;"); // Should print "4" to the console
-// ...
-```
-
-To do so, you require a parser, interpreter and console for output. Note that, as demonstrated below, it can still make sense to create "hard dependencies" outside of the IoC container, eg. for FlowProgram below. A typical setup may look like the following:
-
-```javascript
 /*
  * NB: Named defines will be used to encapsulate the module folder structure within this one file.
  *     Ordinarily, the definitions below would be anonymous and stored in separate module files:
@@ -245,6 +195,3 @@ require([
             vm.run("print 47;");  // Will print "47" to the console
         });
 });
-```
-
-Note that the actual construction of the service objects ("parser", "interpreter", "console" and "vm") is handled by Service.JS.
